2755a30aa13cde67870526ce4b5b980dc52921b2
lisa <lisaauta018@gmail.com>
2025-03-02 09:40:03 +0300

    navs work

--- App.jsx
+++ App.jsx
@@ -1,31 +1,22 @@
+// App.js
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomePage';
+import DetailsScreen from './screens/details';
+
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}> 
-      <Text style={styles.instructions}>Hello lisa</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-})
 
 export default App;
